544cdce8030b93313332790704597dd305adc4cf
Jacob K <jkocina.jr@gmail.com>
2016-10-07 11:56:45 -0500

    Improving Usability
    Instead of the file name "demo" and a time stamp. Excel Files are now output with the choice of file names and time stamp

--- csvToXlsx.py
+++ csvToXlsx.py
@@ -6,20 +6,24 @@ import time
 recordCount = 1
 
 file = open(sys.argv[1], 'r')
-workbook = xlsxwriter.Workbook('demo' + str(time.time()) + '.xlsx')
+if sys.argv[3] = true: 
+    workbook = xlsxwriter.Workbook(sys.argv[2] + str(datetime.datetime()) + '.xlsx')
+else:    
+    workbook = xlsxwriter.Workbook(sys.argv[2] + '.xlsx')	
+
 worksheet = workbook.add_worksheet()
 
 try:
-	reader = csv.reader(file)
-	for row in reader:
-		print("Record Count: " + str(recordCount))
-		columnCount = "A"
+    reader = csv.reader(file)
+    for row in reader:
+        print("Record Count: " + str(recordCount))
+	    columnCount = "A"
 		
-		for dataPoint in row:
-			#print(dataPoint)
-			worksheet.write(columnCount + str(recordCount), dataPoint)
-			columnCount = chr(ord(columnCount) + 1)
-		recordCount += 1
+	    for dataPoint in row:
+	        #print(dataPoint)
+		worksheet.write(columnCount + str(recordCount), dataPoint)
+		columnCount = chr(ord(columnCount) + 1)
+	    recordCount += 1
 	
 finally:
 	file.close()	
