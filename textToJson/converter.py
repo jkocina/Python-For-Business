72f19b5f5b35ac884a044a11843ffbc13bb2150d
Jacob Kocina <9373074+jkocina@users.noreply.github.com>
2020-04-26 16:33:57 -0500

    txt file apart of Text to Json completed in converter

--- textToJson/converter.py
+++ textToJson/converter.py
@@ -1,75 +1,66 @@
-# Python program to convert text 
-# file to JSON 
-  
-  
-import json 
+# Python script to convert a txt doc of stock tickers to json
+# file to JSON
+import json
+import pprint
 
+pp = pprint.PrettyPrinter(indent = 4)
 
 def textToJson():
-    
-    # the file to be converted 
+
+    # the file to be converted
     tickersLocation = 'NYSE.txt'
-    
-    # intermediate and resultant dictionaries 
-    # intermediate 
-    dict2 = {} 
-    
-    # resultant 
-    dict1 = {} 
-    
-    # fields in the sample file  
-    fields =['symbol', 'description'] 
-    
-    with open(tickersLocation) as tickers: 
-        
-        # loop variable 
+
+    # intermediate and resultant dictionaries
+    # intermediate
+    dict2 = {}
+
+    # resultant
+    dict1 = {}
+
+    # fields in the sample file
+    fields =['symbol', 'description']
+
+    with open(tickersLocation) as tickers:
+
+        # loop variable
         i = 0
-        
-        # count variable for ticker  
+
+        # count variable for ticker
         l = 1
-        
-        for ticker in tickers: 
-            
-            # reading line by line from the text file 
+
+        # reading line by line from the text file
+        for ticker in tickers:
+
+            #removing the new line at the end of each Line
             tickerNoNewLine = ticker.split('\n')
-            
+
+            #spliting symbol and description
             tickerText = list(tickerNoNewLine[0].split('\t'))
-           #print(tickerText[0]) 
-            
-            # for automatic creation of id for each employee 
-            #sno ='T'+str(l) 
-        
-            dict2.update({[fields[0]] : tickerText[0], fields[1] : tickerText[1]})
 
-            # while i<len(fields): 
-                
-            #     # creating dictionary for each employee 
-                
-            #     if i == 0:
-                    
-            #         dict2.update([fields[i]] : tickerText[0] , 
-            #     else:
+            try:
+                dict2[l] = {fields[0] : tickerText[0], fields[1] : tickerText[1]}
+            except:
 
-            #         dict2[l][fields[i]] = tickerText[1]    
+                dict2[l] = {fields[0] : tickerText[0]}
 
-            #     i = i + 1
 
-            #print(dict2)       
-            # appending the record of each employee to 
-            # the main dictionary 
-            dict1.update(dict2)
+            # appending the record of each employee to
+            # the main dictionary
+            #dict1.update(dict2)
             l = l + 1
-            i = 0
 
+
+    #printing out the dictionaries
     #print(dict1)
-    print(dict2)    
-    # creating json file         
-    # out_file = open("tickers.json", "w") 
-    # json.dump(dict1, out_file, indent = 4) 
-    # out_file.close()
+    pp.pprint(dict2)
+
+    # creating json file
+    out_file = open("tickers.json", "w")
+    json.dump(dict2, out_file, indent = 4)
+    out_file.close()
 
 
-def htmlToJSON():
+#def htmlToJSON():
     # import html
 
     # use dom to find table root
@@ -82,18 +73,18 @@ def htmlToJSON():
     # for each tableHeader
     #     headerArray += tableHeader
 
-    
+
     # for each Record
-        
+
     #     let r = 0
-        
+
     #     for each tableheader
 
     #         for each dataPoint
 
     #         completeArray[r][tableheader] = dataPoint
-        
-        
+
+
     #     r = r + 1
 
 
